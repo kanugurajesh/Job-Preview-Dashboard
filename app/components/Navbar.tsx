4fdaab7e869254295cb38a757dcb3f0859c5b340
kanugurajesh <kanugurajesh3@gmail.com>
2024-06-25 18:27:00 +0530

    Adding middle navbar elements

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,12 +7,19 @@ const Navbar = () => {
         Logo
       </div>
       <div className="w-[1430px] flex items-center justify-center">
-        <div className="w-[538px] h-[74px] border-2 border-[#D1D1D1] rounded-[36px] ">
-          <div className="px-[109px] py-[58px]">
-            <div className="w-[79px] h-[27px]">
-              <Image src="" alt="" />
-              <p>Jobs</p>
-            </div>
+        <div className="w-[538px] h-[74px] border-2 border-[#D1D1D1] rounded-[36px] flex items-center justify-around">
+          <div className="h-[27px] text-[#B0B0B0] flex gap-[8px] items-center justify-center py-[1.5px]">
+            <Image src="/briefcase.svg" alt="" width={24} height={24} />
+            <p className="text-lg">Jobs</p>
+          </div>
+          <div className="h-[27px] text-[#B0B0B0] flex gap-[8px] items-center justify-center py-[1.5px] relative">
+            <Image src="/message-square.svg" alt="" width={24} height={24} />
+            <div className="absolute w-[6px] h-[6px] bg-[#DC4A2D] rounded-full top-[1px] left-[18px]"></div>
+            <p className="text-lg">Messages</p>
+          </div>
+          <div className="h-[27px] text-[#B0B0B0] flex gap-[8px] items-center justify-center py-[1.5px]">
+            <Image src="/coins-hand.svg" alt="" width={28} height={28} />
+            <p className="text-lg">Payments</p>
           </div>
         </div>
       </div>
